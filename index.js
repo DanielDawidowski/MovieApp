d7202298b5d54aaee3ea63a5f1c447fa11328cd4
Daniel Dawidowski <dvds1987@gmail.com>
2020-01-13 18:14:59 +0100

    Parsing Numbers of Awards

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ createAutoComplete({
     root: document.querySelector('#left-autocomplete') ,
     onOptionSelect(movie) {
         document.querySelector('.tutorial').classList.add('is-hidden');
-        onMovieSelect(movie, document.querySelector('#left-summary'));
+        onMovieSelect(movie, document.querySelector('#left-summary'), 'left');
     },
 })
 
@@ -39,13 +39,14 @@ createAutoComplete({
     root: document.querySelector('#right-autocomplete'),
     onOptionSelect(movie) {
         document.querySelector('.tutorial').classList.add('is-hidden');
-        onMovieSelect(movie,  document.querySelector('#right-summary'));
+        onMovieSelect(movie,  document.querySelector('#right-summary'), 'right');
     }, 
 })
 
 
-
-const onMovieSelect = async (movie, summaryElement) => {
+let leftMovie;
+let rightMovie;
+const onMovieSelect = async (movie, summaryElement, side) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: 'e375bd90',
@@ -55,10 +56,41 @@ const onMovieSelect = async (movie, summaryElement) => {
 
     summaryElement.innerHTML = movieTemplate(response.data);
     // console.log(response.data)
+
+    if(side === 'left') {
+        leftMovie = response.data; 
+    } else {
+        rightMovie = response.data;
+    }
+
+    if ( leftMovie && rightMovie) {
+        runComparison()
+    }
 }
 
+const runComparison = () => {
+    console.log('Time for Comparison')
+}
 
 const movieTemplate = movieDetail => {
+    const dollars = parseInt(movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/g, ''));
+    const metascore = parseInt(movieDetail.Metascore);
+    const imdbRating = parseFloat(movieDetail.imdbRating);
+    const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ''));
+    
+    const awards = movieDetail.Awards.split(' ').reduce((prev, word) => {
+        const value = parseInt(word);
+
+        if (isNaN(value)) {
+            return prev;
+        } else {
+            return prev + value;
+        }
+        
+    }, 0)
+    console.log(awards);
+    
+
     return `
         <article class="media">
             <figure class="media-left">
